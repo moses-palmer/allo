3d683c1487eb6d587c98fac73d23f2e8f04a26ff
Moses Palmér <mosespalmer@gmail.com>
2021-05-01 00:00:00 +0000

    Added dummy email sender

--- backend/src/email/mod.rs
+++ backend/src/email/mod.rs
@@ -3,3 +3,9 @@ pub mod template;
 
 pub use self::sender::Sender;
 pub use self::template::Templates;
+
+pub mod dummy;
+pub use dummy as driver;
+
+pub use self::driver::Configuration;
+pub use self::driver::Transport;
