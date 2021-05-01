76846871eefbbc801dc96e7c0b122a8e57dc365c
Moses Palmér <mosespalmer@gmail.com>
2021-05-01 00:00:00 +0000

    Added method to determine mailbox for user

--- backend/src/email/mod.rs
+++ backend/src/email/mod.rs
@@ -1,3 +1,8 @@
+use lettre::message::Mailbox;
+use lettre::Address;
+
+use crate::db::values::EmailAddress;
+
 mod sender;
 pub mod template;
 
@@ -16,3 +21,30 @@ pub use smtp as driver;
 
 pub use self::driver::Configuration;
 pub use self::driver::Transport;
+
+/// Creates a mailbox for a named user with an email address.
+///
+/// # Arguments
+/// *  `name` - The user name.
+/// *  `email` - The user email address. If this cannot be converted to a
+///    mailbox address, nothing is returned.
+pub fn mailbox(name: &str, email: &EmailAddress) -> Option<Mailbox> {
+    email
+        .to_string()
+        .parse::<Address>()
+        .ok()
+        .map(|address| Mailbox::new(Some(name.into()), address))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn mailbox_valid() {
+        assert_eq!(
+            "Test <test@example.com>".parse::<Mailbox>().ok(),
+            mailbox("Test", &"test@example.com".parse().unwrap()),
+        );
+    }
+}
