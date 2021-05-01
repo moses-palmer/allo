fb4e2bfb36fb99249b1f3e055e6483a9bd9fe274
Moses Palmér <mosespalmer@gmail.com>
2021-05-01 00:00:00 +0000

    Let clients know whether email is configured

--- backend/src/api/server/mod.rs
+++ backend/src/api/server/mod.rs
@@ -8,6 +8,10 @@ use crate::api;
 pub async fn handle() -> impl Responder {
     api::ok(Res {
         version: env!("CARGO_PKG_VERSION").into(),
+        features: vec![
+            #[cfg(feature = "email_smtp")]
+            "email".into(),
+        ],
     })
 }
 
@@ -15,4 +19,7 @@ pub async fn handle() -> impl Responder {
 pub struct Res {
     /// The server version.
     version: String,
+
+    /// The features enabled for this server.
+    features: Vec<String>,
 }
