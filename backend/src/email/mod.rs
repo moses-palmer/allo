dd7b6a60f691f472f2d516ca240c3393432128d2
Moses Palmér <mosespalmer@gmail.com>
2021-05-01 00:00:00 +0000

    Added SMTP email sender

--- backend/src/email/mod.rs
+++ backend/src/email/mod.rs
@@ -4,8 +4,15 @@ pub mod template;
 pub use self::sender::{Error, Sender};
 pub use self::template::Templates;
 
+#[cfg(not(feature = "email_smtp"))]
 pub mod dummy;
+#[cfg(not(feature = "email_smtp"))]
 pub use dummy as driver;
+
+#[cfg(feature = "email_smtp")]
+pub mod smtp;
+#[cfg(feature = "email_smtp")]
+pub use smtp as driver;
 
 pub use self::driver::Configuration;
 pub use self::driver::Transport;
