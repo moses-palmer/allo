131241fd9f5a90108ee081c9f007f3d5a0f941b1
Moses Palmér <mosespalmer@gmail.com>
2021-05-01 00:00:00 +0000

    Added base for allowance application builder

---
+++ backend/src/main.rs
@@ -0,0 +1,34 @@
+use actix::prelude::*;
+
+use std::io;
+use std::process::exit;
+
+use actix_web::{App, HttpServer};
+use env_logger;
+
+async fn run() -> io::Result<()> {
+    env_logger::builder().format_timestamp(None).init();
+
+    HttpServer::new(|| App::new())
+        .bind("0.0.0.0:8000")
+        .unwrap()
+        .run()
+        .await
+}
+
+#[actix_web::main]
+async fn main() {
+    use std::error::Error;
+    match run().await {
+        Err(e) => {
+            eprintln!("Failed to run: {}", e);
+            let mut error = e.source();
+            while let Some(e) = error {
+                eprintln!("Caused by: {}", e);
+                error = e.source();
+            }
+            exit(1);
+        }
+        Ok(_) => {}
+    }
+}
